48588758d930d1a8d85d124129bb6bd08c4025fb
Calvin Hamus <calvin.hamus@aeengr.com>
2022-05-23 12:22:36 -0500

    Add project files.

---
+++ Data/CodeingTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CodeingTest.Models;
+
+namespace CodeingTest.Data
+{
+    public class CodeingTestContext : DbContext
+    {
+        public CodeingTestContext (DbContextOptions<CodeingTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CodeingTest.Models.Employee>? Employee { get; set; }
+    }
+}
---
+++ Data/FakeData.cs
@@ -0,0 +1,21 @@
+﻿using CodeingTest.Models;
+
+namespace CodeingTest.Data
+{
+    public class FakeData
+    {
+        private List<Employee> Employees;
+        public FakeData()
+        {
+            Employees = new List<Employee>();
+            Employees.Add(new Employee { Name= "Calvin", EmployeeId = 1, Address="Stevens Point"});
+            Employees.Add(new Employee { Name = "Lauren", EmployeeId = 2, Address = "Boise" });
+            Employees.Add(new Employee { Name = "Chris", EmployeeId = 3, Address = "Greer" });
+        }
+        public List<Employee> GetEmployees()
+        {
+            return Employees;
+        }
+
+    }
+}
